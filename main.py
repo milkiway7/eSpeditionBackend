16647aa03ba23d508e1370bb13cfbcffdcc43829
alexm <alex.mleczkomt@gmail.com>
2025-12-13 11:50:52 +0100

    First push

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from dotenv import load_dotenv
 load_dotenv()
+
 import uvicorn
 from fastapi import FastAPI
 from Api import location_api, user_api
-from contextlib import asynccontextmanager 
+from contextlib import asynccontextmanager
 from DataBase.database_initialization import initialize_database
+from fastapi.middleware.cors import CORSMiddleware
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -13,13 +15,21 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(location_api.router)
 app.include_router(user_api.router)
 
 if __name__ == "__main__":
     uvicorn.run(
         "main:app",
-        host="0.0.0.0",
+        host="localhost",
         port=8000,
         reload=True
     )
