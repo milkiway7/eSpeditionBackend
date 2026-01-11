500bf8691fd4fc575f5e2e96e36d882efc0634a3
alexm <alex.mleczkomt@gmail.com>
2026-01-11 16:09:12 +0100

    Udate FSM and registrations enum. Add DETAILS_COMPLETED

--- Domain/Fsm/registration_fsm.py
+++ Domain/Fsm/registration_fsm.py
@@ -2,9 +2,10 @@ from Enums.registration_status import RegistrationStatus
 
 ALLOWED_TRANSITIONS = {
     RegistrationStatus.KRS_VERIFIED: {
-        RegistrationStatus.ROLE_SELECTED,
-        RegistrationStatus.COMPLETED,
-        RegistrationStatus.FAILED
+        RegistrationStatus.DETAILS_COMPLETED
+    },
+    RegistrationStatus.DETAILS_COMPLETED: {
+        RegistrationStatus.ROLE_SELECTED
     },
     RegistrationStatus.ROLE_SELECTED: {
         RegistrationStatus.COMPLETED,
